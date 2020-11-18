4b9e9d10a762891799b6ae4095cc0905f108b121
Mattt <mattt@me.com>
2020-11-18 09:33:27 -0800

    Exit on subshell failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,8 +47,8 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git init
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-    git pull "$GIT_REPOSITORY_URL" || exit 1
-)
+    git pull "$GIT_REPOSITORY_URL"
+) || exit 1
 
 debug "Enumerating contents of $1"
 for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
@@ -61,8 +61,8 @@ debug "Committing and pushing changes"
     cd "$tmp_dir" || exit 1
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
-    git push --set-upstream "$GIT_REPOSITORY_URL" master || exit 1
-)
+    git push --set-upstream "$GIT_REPOSITORY_URL" master
+) || exit 1
 
 rm -rf "$tmp_dir"
 exit 0
